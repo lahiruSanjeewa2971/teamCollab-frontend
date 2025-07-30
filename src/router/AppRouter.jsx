156e11b475205486ce2666782ed36fb731c13a1c
lahiruR <lahiru.rathnasheela@synapsys.lk>
2025-07-30 23:37:56 +0530

    First commit of the project - setup tailwindcss and necessary packages | start routes and redux setup

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter, Routes } from 'react-router-dom'
+
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+        <Routes>
+
+            {/* Public routes */}
+
+            {/* Protected routes */}
+        </Routes>
+    </BrowserRouter>
+  )
+}
